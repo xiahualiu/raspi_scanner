b74da87b1dcb790c5bd8b0ea9ecf12cc3b36afd6
xiahualiu <49677912+xiahualiu@users.noreply.github.com>
2021-10-29 16:09:58 -0400

    Add script.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # raspi_scanner
 A bash script that uses `nmap` to automatically scan the Raspberry Pi on the same subnet.
+
+# Example
+
+```bash
+$ bash ./scan_pi.sh
+Your IP is 172.16.3.157
+Your IP with prefix is 172.16.3.157/23
+============================================
+MAC Name: Raspberry Pi Trading
+      IP: 172.16.3.142
+```
---
+++ scan_pi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Find IP
+IP=$(hostname -I | sed "s/\s//g")
+echo -e "Your IP is $IP"
+# Escape .
+IP=$(echo -ne "$IP" | sed 's/\./\\\./g')
+
+# Find IP with prefix length
+IPP=$(ip addr | sed -n "s/.*\($IP\/[^ ]*\).*/\1/p")
+echo -e "Your IP with prefix is $IPP"
+echo "============================================"
+
+# Nmap 
+Result=$(sudo nmap -sP $IPP | tr '\n' ',' | sed -n 's/.*\(Raspberry[^,]\+,[^,]\+\).*/\1/p')
+echo "MAC Name: $(echo -ne "$Result" | sed -n 's/\([^)]*\).*/\1/p')"
+echo "      IP: $(echo -ne "$Result" | sed -n 's/.*\s\([0-9.]*\)$/\1/p')"
